80542b18d60d1fa8f5488ad4ebf5d78a38eb69b7
Ashton Mchenry <kingspandex0@gmail.com>
2024-09-13 12:21:03 -0400

    .

--- Machine Learning Practice/Machine_Learning_Practice.py
+++ Machine Learning Practice/Machine_Learning_Practice.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import math
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import train_test_split as tts
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 # Set display options to show all columns
 pd.set_option('display.max_columns', None)
@@ -51,5 +52,34 @@ print(melbourne_model.predict(X.head()))
 
 # Mean absolute Error (MAE)
 predicted_home_prices = melbourne_model.predict(X)
-mae = tts.mean_absolute_error(y, predicted_home_prices)
-print('\nMean Absolute Error (MAE): ', mae)
+print('\nMean absolute error:')
+print(mean_absolute_error(y, predicted_home_prices))
+
+# Importance of train test split
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print('\nTrain test split :')
+print(mean_absolute_error(val_y, val_predictions))
+
+#overfitting vs underfitting (controlling tree depth)
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
